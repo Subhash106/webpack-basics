bc5861c2d82a37d284f2c7b20290df3cdccd83e6
Subash Chandra <subash_chandra@crgl-thirdparty.com>
2023-02-18 08:15:56 +0530

    loading images

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import _ from "lodash";
 import "./style.css";
+import logo from "./images/instagram_logo_2000.png";
 
 function component() {
   const element = document.createElement("div");
 
   element.innerHTML = _.join(["Hello", "Webpack", ""]);
   element.classList.add("hello");
+
+  // create image element
+  const image = new Image();
+  image.src = logo;
+  image.classList.add("logo");
+  element.appendChild(image);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,19 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "dist")
   },
   mode: "development",
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ["style-loader", "css-loader"]
       },
-    ],
-  },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i,
+        type: "asset/resource"
+      }
+    ]
+  }
 };
